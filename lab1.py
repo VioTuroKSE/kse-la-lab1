ed10d9d3d36ed717954a75ee1142e3c2dd9531c5
VioTuro <atkachov@kse.org.ua>
2024-06-02 23:46:24 +0300

    Made at least something :')

--- lab1.py
+++ lab1.py
@@ -5,9 +5,25 @@ import matplotlib.pyplot as plt
 print("--- PART 1 ---")
 
 BATMAN = np.array([[0, 0], [1, 0.2], [0.4, 1], [0.5, 0.4], [0, 0.8], [-0.5, 0.4], [-0.4, 1], [-1, 0.2], [0, 0]])
+NOT_A_BATMAN = np.array([[0,1], [2,3], [5,3], [3,1]])
+
 plt.plot(BATMAN)
 plt.show()
 
-print("--- PART 2 ---")
+plt.plot(NOT_A_BATMAN)
+plt.show()
 
+def rotate(array: np.ndarray, angle: int):
+    angle = angle*np.pi/180
+    i = 0
+    for vector in array:
+        array[i] = vector @ np.array([[np.cos(angle), -np.sin(angle)], [np.sin(angle), np.cos(angle)]])
+        i+=1
+    return array
+
+rot_batman = rotate(BATMAN, 90)
+plt.plot(rot_batman)
+plt.show()
+
+print("--- PART 2 ---")
 
