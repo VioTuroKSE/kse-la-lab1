dd97415f304aa75fdcc8ceaad49d39efb9ceb4d7
VioTuro <atkachov@kse.org.ua>
2024-06-02 23:07:05 +0300

    Implemented work with numpy&matplotlib

---
+++ lab1.py
@@ -0,0 +1,13 @@
+import numpy as np
+import cv2 as cv
+import matplotlib.pyplot as plt
+
+print("--- PART 1 ---")
+
+BATMAN = np.array([[0, 0], [1, 0.2], [0.4, 1], [0.5, 0.4], [0, 0.8], [-0.5, 0.4], [-0.4, 1], [-1, 0.2], [0, 0]])
+plt.plot(BATMAN)
+plt.show()
+
+print("--- PART 2 ---")
+
+
